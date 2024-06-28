d050e114a560de345b8bdb75c9620801e58c6bf2
Richard Seal <richyseal@gmail.com>
2024-06-27 22:16:48 -0400

    I hate this linter

--- src/sxastarter/src/Header.tsx
+++ src/sxastarter/src/Header.tsx
@@ -12,36 +12,38 @@ import Scripts from 'src/Scripts';
 const publicUrl = config.publicUrl;
 
 interface LayoutProps {
-    layoutData: LayoutServiceData;
-    headLinks: HTMLLink[];
+  layoutData: LayoutServiceData;
+  headLinks: HTMLLink[];
 }
 
 interface RouteFields {
-    [key: string]: unknown;
-    Title?: Field;
+  [key: string]: unknown;
+  Title?: Field;
 }
 
 const Header = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
-    const { route } = layoutData.sitecore;
-    const fields = route?.fields as RouteFields;
+  const { route } = layoutData.sitecore;
+  const fields = route?.fields as RouteFields;
 
-    return (
-        <>
-            <Scripts />
-            <Head>
-                <title>{fields?.Title?.value?.toString() || 'Page'}</title>
-                <link rel="icon" href={`${publicUrl}/favicon.ico`} />
-                {headLinks.map((headLink) => (
-                    <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
-                ))}
-            </Head>
+  return (
+    <>
+      <Scripts />
+      <Head>
+        <title>{fields?.Title?.value?.toString() || 'Page'}</title>
+        <link rel="icon" href={`${publicUrl}/favicon.ico`} />
+        {headLinks.map((headLink) => (
+          <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
+        ))}
+      </Head>
 
-            {/* root placeholder for the app, which we add components to using route data */}
-            <header>
-                <div id="header" className="row">{route && <Placeholder name="headless-header" rendering={route} />}</div>
-            </header>
-        </>
-    );
+      {/* root placeholder for the app, which we add components to using route data */}
+      <header>
+        <div id="header" className="row">
+          {route && <Placeholder name="headless-header" rendering={route} />}
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
--- src/sxastarter/src/Layout.tsx
+++ src/sxastarter/src/Layout.tsx
@@ -2,7 +2,7 @@
  * This Layout is needed for Starter Kit.
  */
 import React from 'react';
-import { Placeholder, LayoutServiceData, Field, HTMLLink } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Placeholder, LayoutServiceData, HTMLLink } from '@sitecore-jss/sitecore-jss-nextjs';
 //import config from 'temp/config';
 
 // Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
@@ -10,8 +10,8 @@ import { Placeholder, LayoutServiceData, Field, HTMLLink } from '@sitecore-jss/s
 //const publicUrl = config.publicUrl;
 
 interface LayoutProps {
-    layoutData: LayoutServiceData;
-    headLinks: HTMLLink[];
+  layoutData: LayoutServiceData;
+  headLinks: HTMLLink[];
 }
 
 /*
@@ -22,20 +22,20 @@ interface RouteFields {
 */
 
 const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
-    const { route } = layoutData.sitecore;
-    //const fields = route?.fields as RouteFields;
+  const { route } = layoutData.sitecore;
+  //const fields = route?.fields as RouteFields;
 
-    return (
-        <>
-            {/* root placeholder for the app, which we add components to using route data */}
-            <main>
-                <div id="content">{route && <Placeholder name="headless-main" rendering={route} />}</div>
-            </main>
-            <footer>
-                <div id="footer">{route && <Placeholder name="headless-footer" rendering={route} />}</div>
-            </footer>
-        </>
-    );
+  return (
+    <>
+      {/* root placeholder for the app, which we add components to using route data */}
+      <main>
+        <div id="content">{route && <Placeholder name="headless-main" rendering={route} />}</div>
+      </main>
+      <footer>
+        <div id="footer">{route && <Placeholder name="headless-footer" rendering={route} />}</div>
+      </footer>
+    </>
+  );
 };
 
 export default Layout;
