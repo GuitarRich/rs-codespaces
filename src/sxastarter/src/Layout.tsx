e2d3b48c4fc5cecae0e27447f78dcec6c4ec3871
Richard Seal <richyseal@gmail.com>
2024-06-28 10:58:51 -0400

    Clean up into a single component and add footer in

--- src/sxastarter/src/Layout.tsx
+++ src/sxastarter/src/Layout.tsx
@@ -2,40 +2,59 @@
  * This Layout is needed for Starter Kit.
  */
 import React from 'react';
-import { Placeholder, LayoutServiceData, HTMLLink } from '@sitecore-jss/sitecore-jss-nextjs';
-//import config from 'temp/config';
+import { Placeholder, LayoutServiceData, HTMLLink, Field } from '@sitecore-jss/sitecore-jss-nextjs';
+import Scripts from './Scripts';
+import Head from 'next/head';
+import config from 'temp/config';
 
 // Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
 // If you're not supporting the Experience Editor, you can remove this.
-//const publicUrl = config.publicUrl;
+const publicUrl = config.publicUrl;
 
 interface LayoutProps {
-  layoutData: LayoutServiceData;
-  headLinks: HTMLLink[];
+    layoutData: LayoutServiceData;
+    headerLayoutData: LayoutServiceData;
+    footerLayoutData: LayoutServiceData;
+    headLinks: HTMLLink[];
 }
 
-/*
 interface RouteFields {
     [key: string]: unknown;
     Title?: Field;
 }
-*/
 
-const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
-  const { route } = layoutData.sitecore;
-  //const fields = route?.fields as RouteFields;
+const Layout = ({ layoutData, headerLayoutData, footerLayoutData, headLinks }: LayoutProps): JSX.Element => {
+    const { route } = layoutData.sitecore;
+    const { route: headerRoute } = headerLayoutData.sitecore;
+    const { route: footerRoute } = footerLayoutData.sitecore;
+    const fields = route?.fields as RouteFields;
 
-  return (
-    <>
-      {/* root placeholder for the app, which we add components to using route data */}
-      <main>
-        <div id="content">{route && <Placeholder name="headless-main" rendering={route} />}</div>
-      </main>
-      <footer>
-        <div id="footer">{route && <Placeholder name="headless-footer" rendering={route} />}</div>
-      </footer>
-    </>
-  );
+    return (
+        <>
+            <Scripts />
+            <Head>
+                <title>{fields?.Title?.value?.toString() || 'Page'}</title>
+                <link rel="icon" href={`${publicUrl}/favicon.ico`} />
+                {headLinks.map((headLink) => (
+                    <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
+                ))}
+            </Head>
+
+            {/* root placeholder for the app, which we add components to using route data */}
+            <header>
+                <div id="header" className="row">
+                    {headerRoute && <Placeholder name="headless-header" rendering={headerRoute} />}
+                </div>
+            </header>
+            {/* root placeholder for the app, which we add components to using route data */}
+            <main>
+                <div id="content">{route && <Placeholder name="headless-main" rendering={route} />}</div>
+            </main>
+            <footer>
+                <div id="footer">{footerRoute && <Placeholder name="headless-footer" rendering={footerRoute} />}</div>
+            </footer>
+        </>
+    );
 };
 
 export default Layout;
--- src/sxastarter/src/lib/page-props.ts
+++ src/sxastarter/src/lib/page-props.ts
@@ -1,21 +1,22 @@
 import {
-  DictionaryPhrases,
-  ComponentPropsCollection,
-  LayoutServiceData,
-  SiteInfo,
-  HTMLLink,
+    DictionaryPhrases,
+    ComponentPropsCollection,
+    LayoutServiceData,
+    SiteInfo,
+    HTMLLink,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 
 /**
  * Sitecore page props
  */
 export type SitecorePageProps = {
-  site: SiteInfo;
-  locale: string;
-  dictionary: DictionaryPhrases;
-  componentProps: ComponentPropsCollection;
-  notFound: boolean;
-  headerLayoutData: LayoutServiceData;
-  layoutData: LayoutServiceData;
-  headLinks: HTMLLink[];
+    site: SiteInfo;
+    locale: string;
+    dictionary: DictionaryPhrases;
+    componentProps: ComponentPropsCollection;
+    notFound: boolean;
+    headerLayoutData: LayoutServiceData;
+    footerLayoutData: LayoutServiceData;
+    layoutData: LayoutServiceData;
+    headLinks: HTMLLink[];
 };
